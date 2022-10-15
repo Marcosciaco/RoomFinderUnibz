0423b490b6b7ae58d3837ca22aae97bca11b3cda
Marco Sciacovelli <36106337+Marcosciaco@users.noreply.github.com>
2022-10-16 00:55:11 +0200

    Updated PWA settings and added custom icons as well as analytics

--- src/main.js
+++ src/main.js
@@ -1,15 +1,37 @@
-import Vue from 'vue'
-import App from './App.vue'
-import './registerServiceWorker'
-import router from './router'
-import store from './store'
-import vuetify from './plugins/vuetify'
+import Vue from "vue";
+import App from "./App.vue";
+import "./registerServiceWorker";
+import router from "./router";
+import store from "./store";
+import vuetify from "./plugins/vuetify";
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
 
-Vue.config.productionTip = false
+const firebaseConfig = {
+    apiKey: "AIzaSyBECZKutXFCjK3il7lp3_RzRa8_PbodYFo",
+    authDomain: "roomfinderunibz.firebaseapp.com",
+    databaseURL:
+        "https://roomfinderunibz-default-rtdb.europe-west1.firebasedatabase.app",
+    projectId: "roomfinderunibz",
+    storageBucket: "roomfinderunibz.appspot.com",
+    messagingSenderId: "132303071776",
+    appId: "1:132303071776:web:d495cef6f4eb3d87502a45",
+    measurementId: "G-4S5QDVH7YG",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+
+Vue.prototype.$analytics = analytics;
+
+Vue.config.productionTip = false;
 
 new Vue({
-  router,
-  store,
-  vuetify,
-  render: function (h) { return h(App) }
-}).$mount('#app')
+    router,
+    store,
+    vuetify,
+    render: function (h) {
+        return h(App);
+    },
+}).$mount("#app");
--- src/plugins/vuetify.js
+++ src/plugins/vuetify.js
@@ -1,7 +1,40 @@
-import Vue from 'vue';
-import Vuetify from 'vuetify/lib/framework';
+import Vue from "vue";
+import Vuetify from "vuetify/lib/framework";
 
 Vue.use(Vuetify);
 
 export default new Vuetify({
+    theme: {
+        options: {
+            customProperties: true,
+        },
+        themes: {
+            light: {
+                base: "#FBFDFF",
+                primary: "#007BE2",
+                secondary: "#7aafff",
+                tertiary: "#AECFFF",
+                white: "#FBFDFF",
+                black: "#2B363F",
+                red: "#FF4438",
+                green: "#38FF88",
+                light: "#F3F8FF",
+                dark: "#384956",
+                grey: "#D9D9D9",
+            },
+            dark: {
+                base: "#2B363F",
+                primary: "#007BE2",
+                secondary: "#7aafff",
+                tertiary: "#AECFFF",
+                white: "#333333",
+                black: "#FBFDFF",
+                red: "#FF4438",
+                green: "#38FF88",
+                light: "#222222",
+                dark: "#F3F8FF",
+                grey: "#D9D9D9",
+            },
+        },
+    },
 });
